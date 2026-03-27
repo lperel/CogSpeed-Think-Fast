19e5cae78ad5a48c9e0e732ab764e9bbb7e21690
Layne <lperel@me.com>
2026-03-27 14:08:23 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "cogspeed-v21-shell-v15";
+const CACHE_NAME = "cogspeed-v21-shell-v17";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./app.js?v=86",
+  "./app.js?v=89",
   "./manifest.json",
   "./privacy.html",
   "./terms.html",
