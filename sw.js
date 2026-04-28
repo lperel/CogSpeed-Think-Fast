3813662e506e47a5ee1cb69d442528d8375db675
Layne <lperel@me.com>
2026-04-28 16:23:09 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Keep RELEASE in sync with APP_REV_STAMP / DISPLAY_VERSION in app.js and the versioned app.js query in index.html.
-const RELEASE = "699rev177";
+const RELEASE = "699rev178";
 const CACHE_NAME = `cogspeed-v${RELEASE}-shell-v${RELEASE}`;
 const APP_SHELL = [
   "./",
